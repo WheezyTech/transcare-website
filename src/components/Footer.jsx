4ba704180c2535b444f642c64ff6d56dd09d7164
Mose Odhiambo <moseodhiambo26@gmail.com>
2026-03-12 10:54:05 +0300

    Update social media links in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -52,20 +52,20 @@ const Footer = () => {
             <h4>Follow Us</h4>
             <div className="footer-social">
               <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-  <FaFacebookF />
-</a>
+                <FaFacebookF />
+              </a>
 
-<a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
-  <FaTwitter />
-</a>
+              <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+                <FaTwitter />
+              </a>
 
-<a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
-  <FaLinkedinIn />
-</a>
+              <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+                <FaLinkedinIn />
+              </a>
 
-<a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-  <FaInstagram />
-</a>
+              <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+                <FaInstagram />
+              </a>
             </div>
           </div>
 
